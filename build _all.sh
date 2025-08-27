1a3a787f742b9b7b784065841754d6abcd703735
Jaggernaute <alex.videcoq@etudiant.univ-rennes.com>
2025-08-28 01:11:06 +0200

    cleanup useless build files

--- build _all.sh
+++
@@ -1,6 +0,0 @@
-cd src/ &&
-find . -type d -exec mkdir -p -- output/{} \
-
-find . -name '*.tex' | while read -r texfile; do
-  make4ht -u -e build.lua "$texfile"
-done
--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-OUTPUT_DIR="output"
-
-echo "🧹 Cleaning LaTeX temp files..."
-rm -f *.aux *.log *.toc *.out *.dvi *.4ct *.4tc *.idv *.lg *.tmp *.xref *.css *.html *.png *.bcf *.xml *.bbl *.blg *.bib
-
-echo "✅ Done!"
